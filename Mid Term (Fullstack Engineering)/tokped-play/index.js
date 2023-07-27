8dd3b907a4394facb613a36b6b2a11dd16d3dc1b
Anjas <anjasopo@gmail.com>
2023-07-27 23:05:00 +0700

    Mid Term Project: Fullstack Engineering

---
+++ Mid Term (Fullstack Engineering)/tokped-play/index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+const routes = require('./src/routes/routes');
+
+mongoose.connect(mongoString);
+
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+database.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use('/api', routes);
+
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
